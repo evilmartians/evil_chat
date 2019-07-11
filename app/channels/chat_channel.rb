a20e5ffb26e3c9d43adeb3be5cffe7f70066c9d5
Alexey Plutalov <demiazz.py@gmail.com>
2019-07-11 16:23:41 +0300

    Adds business logic to `chat` channel

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,9 +1,18 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "chat"
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  # Called when message-form contents are received by the server
+  def send_message(payload)
+    message = Message.new(author: current_user, text: payload["message"])
+
+    ActionCable.server.broadcast "chat", message: render(message) if message.save
+  end
+
+  private
+
+  def render(message)
+    ApplicationController.new.helpers.c("message", message: message)
   end
 end
