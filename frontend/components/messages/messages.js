16faa03214cf9ae68ddd918ceb8a27068664489d
Alexey Plutalov <demiazz.py@gmail.com>
2019-07-11 17:02:09 +0300

    Adds handling of new messages

--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,2 +1,24 @@
+import { setCallback } from "client/chat";
 import "components/message/message"; // message is nested, so we import it here
 import "./messages.pcss";
+
+const scrollToBottom = element => {
+  // eslint-disable-next-line
+  element.scrollTop = element.scrollHeight;
+};
+
+const messages = document.querySelector(".js-messages");
+
+if (messages) {
+  const content = messages.querySelector(".js-messages--content");
+
+  scrollToBottom(content);
+
+  // Telling `chat.js` to call this piece of code whenever a new message is received
+  // over ActionCable
+  setCallback(message => {
+    content.insertAdjacentHTML("beforeend", message);
+
+    scrollToBottom(content);
+  });
+}
