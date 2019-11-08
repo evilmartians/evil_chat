c24f4ea4790e696dc7e7b230f56381d6d146c293
Misha Merkushin <merkushin.m.s@gmail.com>
2019-11-08 15:25:27 +0300

    feat: use session in anycable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -6,11 +6,5 @@ module ApplicationCable
       self.current_user = session.fetch("username", nil)
       reject_unauthorized_connection unless current_user
     end
-
-    # By default we don't have access to the session object
-    # https://edgeguides.rubyonrails.org/action_cable_overview.html#notes
-    def session
-      cookies.encrypted[Rails.application.config.session_options[:key]] || {}
-    end
   end
 end
